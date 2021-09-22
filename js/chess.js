f4cc28874073b16ce549e7081bc3b376cff960d0
Luis Angel Osorio <luis.angel.osorio@outlook.com>
2021-09-22 18:11:53 -0500

    changes

---
+++ js/chess.js
@@ -0,0 +1,85 @@
+$(document).ready(function () {
+    //código a ejecutar cuando el DOM está listo para recibir instrucciones.
+    var indice = 0;
+
+    var config = {
+        position: "start",
+    };
+    var board = Chessboard("myBoard", config);
+
+    let posiciones = [
+        "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR",
+        "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR",
+        "rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR",
+        "rnbqkbnr/pppp1ppp/8/4p3/4P3/5N2/PPPP1PPP/RNBQKB1R",
+        "r1bqkbnr/pppp1ppp/2n5/4p3/4P3/5N2/PPPP1PPP/RNBQKB1R",
+        "r1bqkbnr/pppp1ppp/2n5/4p3/2B1P3/5N2/PPPP1PPP/RNBQK2R",
+        "r1bqk1nr/pppp1ppp/2n5/2b1p3/2B1P3/5N2/PPPP1PPP/RNBQK2R",
+        "r1bqk1nr/pppp1ppp/2n5/2b1p3/2B1P3/2P2N2/PP1P1PPP/RNBQK2R",
+        "r1bqk2r/pppp1ppp/2n2n2/2b1p3/2B1P3/2P2N2/PP1P1PPP/RNBQK2R",
+        "r1bqk2r/pppp1ppp/2n2n2/2b1p3/2BPP3/2P2N2/PP3PPP/RNBQK2R",
+        "r1bqk2r/pppp1ppp/2n2n2/2b5/2BpP3/2P2N2/PP3PPP/RNBQK2R",
+        "r1bqk2r/pppp1ppp/2n2n2/2b5/2BPP3/5N2/PP3PPP/RNBQK2R",
+        "r1bqk2r/pppp1ppp/2n2n2/8/1bBPP3/5N2/PP3PPP/RNBQK2R",
+        "r1bqk2r/pppp1ppp/2n2n2/8/1bBPP3/2N2N2/PP3PPP/R1BQK2R",
+        "r1bqk2r/pppp1ppp/2n5/8/1bBPn3/2N2N2/PP3PPP/R1BQK2R",
+        "r1bqk2r/pppp1ppp/2n5/8/1bBPn3/2N2N2/PP3PPP/R1BQ1RK1",
+        "r1bqk2r/pppp1ppp/2n5/8/2BPn3/2b2N2/PP3PPP/R1BQ1RK1",
+        "r1bqk2r/pppp1ppp/2n5/3P4/2B1n3/2b2N2/PP3PPP/R1BQ1RK1",
+        "r1bqk2r/pppp1ppp/2n2b2/3P4/2B1n3/5N2/PP3PPP/R1BQ1RK1",
+        "r1bqk2r/pppp1ppp/2n2b2/3P4/2B1n3/5N2/PP3PPP/R1BQR1K1",
+        "r1bqk2r/ppppnppp/5b2/3P4/2B1n3/5N2/PP3PPP/R1BQR1K1",
+        "r1bqk2r/ppppnppp/5b2/3P4/2B1R3/5N2/PP3PPP/R1BQ2K1",
+    ];
+
+    // posiciones.forEach(function (elemento, indice, array) {
+    //     console.log(elemento, indice);
+    // });
+
+    // console.log(
+    //     "POSITION => ",
+    //     Chessboard.fenToObj("r1bqk2r/ppppnppp/5b2/3P4/2B1R3/5N2/PP3PPP/R1BQ2K1")
+    // );
+
+    function clickInitialPositionBtn() {
+        board.position("start");
+        indice = 0;
+    }
+
+    $("#initialPositionBtn").on("click", clickInitialPositionBtn);
+    $("#antMovimiento").on("click", clickantMovimiento);
+    $("#flipOrientationBtn").on("click", board.flip);
+
+    function clicksigMovimiento() {
+        if (indice < 21) {
+            board.position(posiciones[indice + 1]);
+            indice = indice + 1;
+        }
+    }
+    $("#sigMovimiento").on("click", clicksigMovimiento);
+
+    function clickantMovimiento() {
+        if (indice > 0) {
+            board.position(posiciones[indice - 1]);
+            indice = indice - 1;
+        }
+    }
+
+    function miFuncion(index) {
+        board.position(posiciones[index]);
+        indice = index;
+    }
+
+    var moves = 1;
+    var table = document.querySelector("#table");
+    table.querySelectorAll("td").forEach(function (b) {
+        if (b.id) {
+            b.setAttribute("move", moves);
+            b.addEventListener("click", function () {
+                let move = b.getAttribute("move");
+                miFuncion(move);
+            });
+            moves += 1;
+        }
+    });
+});
